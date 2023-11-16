340067e3d83566440436fbc536d877963519757b
mcavazotti <matheus.d.cavazotti@exxonmobil.com>
2023-11-16 16:51:23 -0300

    Another change

---
+++ internal/repo/functions.go
@@ -0,0 +1,24 @@
+package repo
+
+import (
+	"errors"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func FindRepo(currPath string) (Repository, error) {
+	if _, err := os.Stat(path.Join(currPath, ".git")); errors.Is(err, os.ErrNotExist) {
+		parent, err := filepath.Abs(path.Join(currPath, ".."))
+		if err != nil {
+			panic(err)
+		}
+		if parent == currPath {
+			return Repository{}, errors.New("No git repository.")
+		}
+		return FindRepo(parent)
+
+	} else {
+		return New(currPath), nil
+	}
+}
